92920a65a8ada8005b612be987035fb53a06fb33
unknown <Nova@NOVAJFLAP057.novatendencia.localdomain>
2021-02-23 13:40:30 -0300

    criado servidor UDP

--- src/ServidorPiada.java
+++ src/ServidorPiada.java
@@ -3,6 +3,8 @@ import java.net.*;
 
 public class ServidorPiada {
 
+    private static final String PIADA = "O que cai em pé e corre deitado? R: A chuva!";
+
     public static void main(String[] args) throws IOException {
 
         DatagramSocket datagramSocket = new DatagramSocket(7777);
@@ -14,6 +16,15 @@ public class ServidorPiada {
 
         String msg = new String(datagramPacket.getData());
         System.out.println("A mensagem recebida eh: " + msg);
+
+        InetAddress inetAddress = datagramPacket.getAddress();
+        int porta = datagramPacket.getPort();
+
+        mensagem = PIADA.getBytes();
+
+        datagramPacket = new DatagramPacket(mensagem, mensagem.length, inetAddress, porta);
+
+        datagramSocket.send(datagramPacket);
 
     }
 }
